64d2f76c0f34740450bdaee6fe4f95717235dcd0
Harvard in Tech Seattle <harvardintechseattle@gmail.com>
2021-09-20 15:44:55 -0700

    Update website as of Sep. 20, 2021

---
+++ js/podcast.js
@@ -0,0 +1,41 @@
+$(function () {
+
+    // == Search / Filters ==
+
+    $('.podcast-list').hide();
+
+    // Show grid + reset
+    $('#grid-button').click(function () {
+        $('.podcast-grid').show();
+        $('.podcast-list').hide();
+
+        $('.2021').show();
+        $('.2020').show();
+    });
+
+    // Show list + reset
+    $('#list-button').click(function () {
+        $('.podcast-grid').hide();
+        $('.podcast-list').show();
+
+        $('.2021').show();
+        $('.2020').show();
+    });
+
+    $(".search-button").click(function () {
+        var attr = $(this).attr("data-li");
+
+        if (attr == "2020") {
+            $(".2021").hide();
+            $(".2020").show();
+            
+        } else if (attr == "2021") {
+            $(".2021").show();
+            $(".2020").hide();
+
+        } else {
+            $(".2021").show();
+            $(".2020").show();
+        }
+    });
+});
