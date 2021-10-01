378552b017cb5c6de2fabfdb51ca4272ecf78034
Harvard in Tech Seattle <harvardintechseattle@gmail.com>
2021-09-30 20:54:15 -0700

    Update website as of Sep. 30, 2021

--- js/team.js
+++ js/team.js
@@ -2,6 +2,7 @@ $(function () {
 	$(".page-1").show();
 	$(".page-2").hide();
 	$(".page-3").hide();
+	$(".page-4").hide();
 
     $(".btn").click(function(){
 		var attr = $(this).attr("data-li");
@@ -10,16 +11,26 @@ $(function () {
 			$(".page-1").show();
 			$(".page-2").hide();
 			$(".page-3").hide();
+			$(".page-4").hide();
 		}
 		else if(attr == "page-2"){
 			$(".page-1").hide();
 			$(".page-2").show();
 			$(".page-3").hide();
+			$(".page-4").hide();
 		}
         else if(attr == "page-3"){
 			$(".page-1").hide();
 			$(".page-2").hide();
 			$(".page-3").show();
+			$(".page-4").hide();
+		}
+		else if(attr == "page-4"){
+			$(".page-1").hide();
+			$(".page-2").hide();
+			$(".page-3").hide();
+			$(".page-4").show();
+			
 		}
     })    
 });
